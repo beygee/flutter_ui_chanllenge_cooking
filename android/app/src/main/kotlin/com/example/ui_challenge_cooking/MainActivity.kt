66833e6c36c3362e2b7ecc8804f035c31ed134d8
Beygee <doug0476@naver.com>
2020-05-25 21:39:31 +0900

    Init

---
+++ android/app/src/main/kotlin/com/example/ui_challenge_cooking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_challenge_cooking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
