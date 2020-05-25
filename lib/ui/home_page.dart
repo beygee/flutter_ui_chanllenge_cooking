1b317944ead83c04c06e12a927d1f19334886d97
Beygee <doug0476@naver.com>
2020-05-25 21:59:45 +0900

    접시 돌리기

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:ui_challenge_cooking/models/food.dart';
 
@@ -10,12 +12,37 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   AnimationController _controller;
   int currentIndex = 1;
 
+  // 애니메이션 변수
+  Animation<double> _clockWiseRotationAnimation;
+  Animation<double> _antiClockWiseRotationAnimation;
+  Tween<double> _antiClockWiseRotationTween;
+  Tween<double> _clockWiseRotationTween;
+  bool isClockwise = false;
+  double rotationValue = 0.0;
+
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
+      duration: Duration(milliseconds: 800),
     );
+
+    _clockWiseRotationTween = Tween<double>(end: 2 * pi);
+    _clockWiseRotationAnimation = _clockWiseRotationTween.animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    );
+
+    _antiClockWiseRotationTween = Tween<double>(end: -2 * pi);
+    _antiClockWiseRotationAnimation = _clockWiseRotationTween.animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    )..addListener(() {
+        setState(() {
+          rotationValue = isClockwise
+              ? _clockWiseRotationAnimation.value
+              : _antiClockWiseRotationAnimation.value;
+        });
+      });
   }
 
   @override
@@ -34,7 +61,10 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             color: Color(0xFFF1F1F3),
             child: Stack(
               fit: StackFit.expand,
-              children: <Widget>[_buildPlate()],
+              children: <Widget>[
+                _buildPlate(),
+                _buildGestureDetection(),
+              ],
             ),
           );
         },
@@ -49,14 +79,61 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         offset: Offset(80, 0),
         child: Align(
           alignment: Alignment.centerRight,
-          child: Image.asset(
-            foodList[currentIndex].foodAssetsPath,
-            width: 180,
-            height: 250,
-            fit: BoxFit.fitWidth,
+          child: Transform.rotate(
+            angle: rotationValue,
+            child: Image.asset(
+              foodList[currentIndex].foodAssetsPath,
+              width: 180,
+              height: 250,
+              fit: BoxFit.fitWidth,
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildGestureDetection() {
+    return Positioned(
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: MediaQuery.of(context).size.width / 2,
+      child: GestureDetector(
+        onVerticalDragStart: _onVerticalDragStart,
+        onVerticalDragUpdate: _onVerticalDragUpdate,
+        onVerticalDragEnd: _onVerticalDragEnd,
+      ),
+    );
+  }
+
+  void _onVerticalDragStart(DragStartDetails details) {}
+  void _onVerticalDragUpdate(DragUpdateDetails details) {
+    print(details.delta.dy);
+    isClockwise = details.delta.dy < 0;
+  }
+
+  void _onVerticalDragEnd(DragEndDetails details) {
+    rotationValue = 0.0;
+    _antiClockWiseRotationTween.begin = rotationValue;
+    _clockWiseRotationTween.begin = rotationValue;
+
+    if (!_controller.isAnimating) {
+      _controller.forward(from: 0.0);
+    }
+
+    _changeFood();
+  }
+
+  void _changeFood() {
+    setState(() {
+      if (!isClockwise) {
+        currentIndex =
+            currentIndex < foodList.length - 1 ? currentIndex + 1 : 0;
+      } else {
+        currentIndex =
+            currentIndex > 0 ? currentIndex - 1 : foodList.length - 1;
+      }
+    });
   }
 }
