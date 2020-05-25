433d2b5681b47d33069dd25ca10a45a5da2cd1ef
Beygee <doug0476@naver.com>
2020-05-25 21:48:11 +0900

    홈페이지 생성

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_challenge_cooking/models/food.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,6 +8,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   AnimationController _controller;
+  int currentIndex = 1;
+
   @override
   void initState() {
     super.initState();
@@ -23,6 +26,37 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, widget) {
+          return Container(
+            color: Color(0xFFF1F1F3),
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[_buildPlate()],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildPlate() {
+    //
+    return Container(
+      child: Transform.translate(
+        offset: Offset(80, 0),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Image.asset(
+            foodList[currentIndex].foodAssetsPath,
+            width: 180,
+            height: 250,
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+      ),
+    );
   }
 }
